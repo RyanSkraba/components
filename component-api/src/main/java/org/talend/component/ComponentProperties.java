2bf3e74b0fed8b8d76c60c18bf0ba5b84a19ed7c
Francis Upton IV <fupton@talend.com>
2015-08-25 01:33:30 +0200

    TUP-3313 New component architecture (some documentation)

--- component-api/src/main/java/org/talend/component/ComponentProperties.java
+++ component-api/src/main/java/org/talend/component/ComponentProperties.java
@@ -1,11 +1,33 @@
 package org.talend.component;
 
+import org.talend.component.properties.Property;
+import org.talend.component.properties.layout.Layout;
+
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 /**
+ * The {@code ComponentProperties} class contains the definitions of the properties associated
+ * with a component. These definitions contain enough information to automatically
+ * construct a nice looking user interface (UI) to populate and validate the properties. The objective
+ * is that no actual (graphical) UI code is included in the component's definition
+ * and as well no custom graphical UI is required for most components. The types of UIs that
+ * can be defined include those for desktop (Eclipse), web, and scripting. All of these
+ * will use the code defined here for their construction and validation.
+ * <p>
+ * All aspects of the properties are defined in a subclass of this
+ * class using the {@link Property} and {@link Layout} classes. In addition in cases where
+ * user interface decisions are made in code, methods can be added to the subclass
+ * to influence the flow of the user interface and help with validation.
+ * <p>
+ * Each property can be a Java type, both simple types and collections are permitted.
+ * In addition, {@code ComponentProperties} classes can be composed allowing hierarchies
+ * of properties and collections of properties to be reused.
+ * <p>
+ * Properties can be grouped into forms which can be presented in various ways
+ * by the user interface (for example, a wizard page, a tab in a property sheet, or a dialog).
+ * A wizard can be defined which is a sequence of forms.
  *
- * @author fupton
  */
 
 // @JsonSerialize(using = ComponentPropertiesSerializer.class)
@@ -15,24 +37,8 @@ public abstract class ComponentProperties {
 
     public static final String PAGE_ADVANCED = "advanced";
 
-    protected static final boolean REQUIRED = true;
-
     /**
-     * Unique identification of the this object for the lifetime of the ComponentService. Used to reference the current
-     * value of the ComponentProperties on the server.
-     */
-    protected int id;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    /**
-     * This is called everytime the layout of the component properties needs to be updated
+     * This is called every time the layout of the component properties needs to be updated
      */
     public void refreshLayout() {
         // do nothing by default
@@ -41,12 +47,11 @@ public abstract class ComponentProperties {
     /**
      * Component properties, can we have each property assocaited with multiple pages? There should be a default page
      * which is the standard property sheet (if no other page is associated).
-     *
+     * <p>
      * Split this into component properties which is the actual container, and ComponentPropertiesDesigner which
      * contains the instructions for the designer (which might be updated by the component service).
-     *
+     * <p>
      * Also, abstract schema handling stuff from this.
-     *
      */
 
     ComponentProperties validateProperty(String propName, String value) {
--- component-api/src/main/java/org/talend/component/properties/layout/Layout.java
+++ component-api/src/main/java/org/talend/component/properties/layout/Layout.java
@@ -12,74 +12,38 @@
 // ============================================================================
 package org.talend.component.properties.layout;
 
-/**
- * created by sgandon on 12 août 2015 Detailled comment
- *
- */
 public class Layout {
 
-    private int row = 0;
+    private int row;
 
-    private String group = null;
+    private String group;
 
-    private int order = 0;
+    private int order;
 
     private boolean visible = true;
 
-    /**
-     * Getter for row.
-     * 
-     * @return the row
-     */
     public int getRow() {
         return this.row;
     }
 
-    /**
-     * Sets the row.
-     * 
-     * @param row the row to set
-     * @return
-     */
     public Layout setRow(int row) {
         this.row = row;
         return this;
     }
 
-    /**
-     * Getter for group.
-     * 
-     * @return the group
-     */
     public String getGroup() {
         return this.group;
     }
 
-    /**
-     * Sets the group.
-     * 
-     * @param group the group to set
-     * @return
-     */
     public Layout setGroup(String group) {
         this.group = group;
         return this;
     }
 
-    /**
-     * Getter for order.
-     * 
-     * @return the order
-     */
     public int getOrder() {
         return this.order;
     }
 
-    /**
-     * Sets the order.
-     * 
-     * @param order the order to set
-     */
     public Layout setOrder(int order) {
         this.order = order;
         return this;
@@ -89,11 +53,6 @@ public class Layout {
         return new Layout();
     }
 
-    /**
-     * specify if the associated component should be displayed or not
-     * 
-     * @param b
-     */
     public void setVisible(boolean visible) {
         this.visible = visible;
     }
