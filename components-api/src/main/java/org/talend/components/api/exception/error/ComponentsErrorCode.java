73db404c480e2ac3a75f8721ffe04287630796f6
Sebastien Gandon <sgandon@talend.com>
2015-09-11 10:22:41 +0200

    fixed a version issues on the daikon project

---
+++ components-api/src/main/java/org/talend/components/api/exception/error/ComponentsErrorCode.java
@@ -0,0 +1,94 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2015 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.components.api.exception.error;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.talend.daikon.exception.error.ErrorCode;
+
+/**
+ * Code errors for the Component services
+ */
+public enum ComponentsErrorCode implements ErrorCode {
+
+                                                 WRONG_COMPONENT_NAME(HttpServletResponse.SC_BAD_REQUEST, "name"), //$NON-NLS-1$
+                                                 WRONG_WIZARD_NAME(HttpServletResponse.SC_BAD_REQUEST, "name"); //$NON-NLS-1$
+
+    /** The http status to use. */
+    private int httpStatus;
+
+    /** Expected entries to be in the context. */
+    private List<String> expectedContextEntries;
+
+    /**
+     * default constructor.
+     * 
+     * @param httpStatus the http status to use.
+     */
+    ComponentsErrorCode(int httpStatus) {
+        this.httpStatus = httpStatus;
+        this.expectedContextEntries = Collections.emptyList();
+    }
+
+    /**
+     * default constructor.
+     *
+     * @param httpStatus the http status to use.
+     */
+    ComponentsErrorCode(int httpStatus, String... contextEntries) {
+        this.httpStatus = httpStatus;
+        this.expectedContextEntries = Arrays.asList(contextEntries);
+    }
+
+    /**
+     * @return the product. Default value is Talend.
+     */
+    @Override
+    public String getProduct() {
+        return "Talend"; //$NON-NLS-1$
+    }
+
+    /**
+     * @return the group. Default Value is ALL
+     */
+    @Override
+    public String getGroup() {
+        return "ALL"; //$NON-NLS-1$
+    }
+
+    /**
+     * @return the http status.
+     */
+    @Override
+    public int getHttpStatus() {
+        return httpStatus;
+    }
+
+    /**
+     * @return the expected context entries.
+     */
+    @Override
+    public Collection<String> getExpectedContextEntries() {
+        return expectedContextEntries;
+    }
+
+    @Override
+    public String getCode() {
+        return this.toString();
+    }
+}
