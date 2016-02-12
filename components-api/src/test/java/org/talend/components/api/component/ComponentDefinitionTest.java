573f4f1ab42eaa0ef497db602c5a4ca5065f41f3
Francis Upton IV <fupton@talend.com>
2016-02-11 16:35:14 -0800

    Move ComponentDefinitionTest to the right package.

--- components-api/src/test/java/org/talend/components/api/component/ComponentDefinitionTest.java
+++ components-api/src/test/java/org/talend/components/api/component/ComponentDefinitionTest.java
@@ -10,7 +10,7 @@
 // 9 rue Pages 92150 Suresnes, France
 //
 // ============================================================================
-package org.talend.components.api.components;
+package org.talend.components.api.component;
 
 import static org.junit.Assert.*;
 
