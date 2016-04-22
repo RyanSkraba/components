74ad52f42e58ecd00e3e4d2b1a48e7040f2abb76
Ivan Honchar <ivan.honchar@synapse.com>
2016-04-22 13:56:42 +0300

    TDI-35890 Implemented common JiraDefinition class, which contains common implementation for all Jira components definition classes

---
+++ components-jira/src/main/java/org/talend/components/jira/JiraDefinition.java
@@ -0,0 +1,49 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.components.jira;
+
+import org.talend.components.api.component.AbstractComponentDefinition;
+
+/**
+ * Jira definition common class. It contains some common definition implementation for all Jira components
+ * 
+ * created by ivan.honchar on Apr 22, 2016
+ */
+public abstract class JiraDefinition extends AbstractComponentDefinition {
+
+    /**
+     * Constructor sets component name
+     * 
+     * @param componentName component name
+     */
+    public JiraDefinition(String componentName) {
+        super(componentName);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getMavenGroupId() {
+        return "org.talend.components";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String getMavenArtifactId() {
+        return "components-jira";
+    }
+
+}
