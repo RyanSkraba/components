accbf0a4ccb1e9f57cbebab418fd7c0ebf39afb0
Sebastien Gandon <sgandon@talend.com>
2016-04-28 16:56:22 +0200

    TDK-29 : remove the title param from Form.

--- components-salesforce/src/main/java/org/talend/components/salesforce/SalesforceBulkProperties.java
+++ components-salesforce/src/main/java/org/talend/components/salesforce/SalesforceBulkProperties.java
@@ -39,14 +39,14 @@ public class SalesforceBulkProperties extends ComponentProperties {
     @Override
     public void setupProperties() {
         super.setupProperties();
-        
+
         waitTimeCheckBatchState.setValue(10000);
     }
-    
+
     @Override
     public void setupLayout() {
         super.setupLayout();
-        Form mainForm = Form.create(this, Form.MAIN, "Salesforce Bulk Properties");
+        Form mainForm = Form.create(this, Form.MAIN);
         mainForm.addRow(concurrencyMode);
         mainForm.addRow(rowsToCommit);
         mainForm.addColumn(bytesToCommit);
