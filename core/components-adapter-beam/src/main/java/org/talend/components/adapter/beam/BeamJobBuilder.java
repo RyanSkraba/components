adc00a5a0462e64e48529445ad47b51c1a1ce8e9
RyanSkraba <rskraba@talend.com>
2016-11-03 16:02:18 +0100

    TFD-254 Add module for including Beam API. (#266)
    * TFD-254 Add module for including Beam API.
    
    * TFD-254 Remove dependency on components-api from beam module.
    
    * TFD-254 Change module name to better reflect its use.  Add comments.
    
    * TFD-254 Put the beam adapter module in a 'core' subdirectory.
    
    * Update version to 0.16.0-SNAPSHOT
    
    * TFD-254 Rename package for beam API interfaces.

---
+++ core/components-adapter-beam/src/main/java/org/talend/components/adapter/beam/BeamJobBuilder.java
@@ -0,0 +1,33 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.components.adapter.beam;
+
+/**
+ * Permit a component runtime instance to build itself into a Beam Pipeline.
+ *
+ * The
+ * {@link org.talend.components.api.RuntimableDefinition#getRuntimeInfo(org.talend.daikon.properties.Properties, Object)}
+ * can be used to create an object instance from a {@link org.talend.daikon.properties.Properties}. If that instance
+ * implements this interface, it knows how to build itself into a Beam {@link org.apache.beam.sdk.Pipeline}.
+ */
+public interface BeamJobBuilder {
+
+    /**
+     * Builds the processing into the Beam job using the context provided.
+     * 
+     * @param ctx All of the information available for the job running in the current Beam
+     * {@link org.apache.beam.sdk.Pipeline}, at the time this component is added. The component is responsible for
+     * keeping the context in a known state.
+     */
+    void build(BeamJobContext ctx);
+}
---
+++ core/components-adapter-beam/src/main/java/org/talend/components/adapter/beam/BeamJobContext.java
@@ -0,0 +1,60 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.components.adapter.beam;
+
+import org.apache.beam.sdk.Pipeline;
+import org.apache.beam.sdk.values.PCollection;
+
+/**
+ * Provides a context for building into the current Beam {@link Pipeline} containing the job.
+ *
+ * This context includes all of the changes that have been added to the pipeline by components up to this point, and any
+ * component building into the context is responsible for adding its results back into this context.
+ */
+public interface BeamJobContext {
+
+    /**
+     * Get the PCollection of data was built into the job by earlier components. This should be called for every
+     * incoming connection into a component. Components that do not have incoming connections can use the
+     * {@link #getPipeline()} method to obtain a starting point.
+     * 
+     * @param linkName The unique name for a connection between two components in the job graph.
+     * @return The PCollection that contains the data that will flow over that connection when the job is run.
+     */
+    PCollection getPCollectionByLinkName(String linkName);
+
+    /**
+     * Save a PCollection of data that is the result of building a component into the job. This should be called exactly
+     * once for every outgoing connection from a component.
+     * 
+     * @param linkName The unique name for a connection between two components in the job graph.
+     * @param pCollection The PCollection that contains the data that will flow over that connection when the job is
+     * run.
+     */
+    void putPCollectionByLinkName(String linkName, PCollection pCollection);
+
+    /**
+     * A port name identifies a connection with relation to a component. For example, the port might be 'IN', 'OUT',
+     * 'MAIN' or 'REJECT', where the link name would be 'row1', 'row2', 'row3'.
+     *
+     * @param portName The names of the connection related to the component.
+     * @return The unique name for a connection between two components in the job graph.
+     */
+    String getLinkNameByPortName(String portName);
+
+    /**
+     * @return The Beam Pipeline currently under construction in the job.
+     */
+    Pipeline getPipeline();
+
+}
