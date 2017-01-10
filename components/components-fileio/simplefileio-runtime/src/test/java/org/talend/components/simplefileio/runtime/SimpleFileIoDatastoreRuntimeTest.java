9788dbbc483f6740852c32c4519c0628a12e1302
RyanSkraba <rskraba@talend.com>
2017-01-10 12:37:50 +0100

    feat(beam): Update stable version to 0.4.0. (#411)
    * feat(beam): Update stable version to 0.4.0.
    
    * fix(rest): remove unused snappy installation for docker.
    
    * feat: update to 0.4.0-TLND
    
    * fix: Use beam artifacts from Talend nexus.

--- components/components-fileio/simplefileio-runtime/src/test/java/org/talend/components/simplefileio/runtime/SimpleFileIoDatastoreRuntimeTest.java
+++ components/components-fileio/simplefileio-runtime/src/test/java/org/talend/components/simplefileio/runtime/SimpleFileIoDatastoreRuntimeTest.java
@@ -65,7 +65,7 @@ public class SimpleFileIoDatastoreRuntimeTest {
         // We test the maven dependencies in this runtime class, where they are available.
         List<URL> dependencies = runtimeInfo.getMavenUrlDependencies();
         assertThat(dependencies, notNullValue());
-        assertThat(dependencies, hasItem(new URL("mvn:org.apache.beam/beam-sdks-java-io-hdfs/0.4.0-incubating-SNAPSHOT/jar")));
+        assertThat(dependencies, hasItem(new URL("mvn:org.apache.beam/beam-sdks-java-io-hdfs/0.4.0-TLND/jar")));
         assertThat(dependencies, hasSize(greaterThan(100)));
     }
 
