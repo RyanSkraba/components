12bcce1cbb2d6c96ba5c80ce699bb97cf785d16a
RyanSkraba <rskraba@talend.com>
2017-02-09 17:34:29 +0100

    fix(TCOMP-476): Ensure that the singleton DefinitionRegistry is locked. (#466)
    * fix(TCOMP-476): Ensure that the singleton DefinitionRegistry is locked.
    
    * feat(TCOMP-476): Provide an initial value for definition registry at startup.

--- services/components-api-service-spi/src/main/java/org/talend/components/service/spi/ServiceSpiFactory.java
+++ services/components-api-service-spi/src/main/java/org/talend/components/service/spi/ServiceSpiFactory.java
@@ -1,6 +1,6 @@
 // ============================================================================
 //
-// Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+// Copyright (C) 2006-2017 Talend Inc. - www.talend.com
 //
 // This source code is available under agreement available at
 // %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
@@ -13,32 +13,46 @@
 package org.talend.components.service.spi;
 
 import java.util.ServiceLoader;
-
 import org.talend.components.api.ComponentInstaller;
 import org.talend.components.api.service.ComponentService;
 import org.talend.components.api.service.common.ComponentServiceImpl;
 import org.talend.components.api.service.common.DefinitionRegistry;
 
+/**
+ * Provide a {@link DefinitionRegistry} and {@link ComponentService} based on the jars currently
+ * available in the classpath.
+ */
 public class ServiceSpiFactory {
 
-    private static DefinitionRegistry defReg;
+  /** Singleton for definition registry.  This will be reloaded if set to null. */
+  private static DefinitionRegistry defReg;
 
-    private static ComponentService componentService;
+  /** Singleton for the component service.  This will be reloaded if set to null. */
+  private static ComponentService componentService;
 
-    public static DefinitionRegistry getDefinitionRegistry() {
-        if (defReg == null) {
-            defReg = new DefinitionRegistry();
-            for (ComponentInstaller installer : ServiceLoader.load(ComponentInstaller.class)) {
-                installer.install(defReg);
-            }
-        }
-        return defReg;
+  static {
+    // Ensure that the definition registry is created when the class is loaded.
+    getDefinitionRegistry();
+  }
+
+  public static DefinitionRegistry getDefinitionRegistry() {
+    if (defReg == null) {
+      // Create a new instance of the definition registry.
+      DefinitionRegistry reg = new DefinitionRegistry();
+      for (ComponentInstaller installer : ServiceLoader.load(ComponentInstaller.class)) {
+        installer.install(reg);
+      }
+      reg.lock();
+      // Only assign it to the singleton after being locked.
+      defReg = reg;
     }
+    return defReg;
+  }
 
-    public static ComponentService getComponentService() {
-        if (componentService == null) {
-            componentService = new ComponentServiceImpl(getDefinitionRegistry());
-        }
-        return componentService;
+  public static ComponentService getComponentService() {
+    if (componentService == null) {
+      componentService = new ComponentServiceImpl(getDefinitionRegistry());
     }
+    return componentService;
+  }
 }
