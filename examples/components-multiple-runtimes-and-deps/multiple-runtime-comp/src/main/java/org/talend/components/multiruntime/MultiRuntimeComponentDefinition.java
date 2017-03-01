5fdd784fbc39cfc355b4a5fc9bd55aee2341d59b
RyanSkraba <rskraba@talend.com>
2017-03-01 15:03:21 +0100

    fix: Remove version from multiruntime example. (#531)

--- examples/components-multiple-runtimes-and-deps/multiple-runtime-comp/src/main/java/org/talend/components/multiruntime/MultiRuntimeComponentDefinition.java
+++ examples/components-multiple-runtimes-and-deps/multiple-runtime-comp/src/main/java/org/talend/components/multiruntime/MultiRuntimeComponentDefinition.java
@@ -75,7 +75,7 @@ public class MultiRuntimeComponentDefinition extends AbstractComponentDefinition
         case VERSION_0_1:
             try {
                 result = new JarRuntimeInfo(
-                        new URL("mvn:org.talend.components/test-multiple-runtime-comp-runtime-v01/0.1.0-SNAPSHOT"),
+                        new URL("mvn:org.talend.components/test-multiple-runtime-comp-runtime-v01"),
                         DependenciesReader.computeDependenciesFilePath("org.talend.components",
                                 "test-multiple-runtime-comp-runtime-v01"),
                         "org.talend.multiruntime.MultiRuntimeComponentSource");
@@ -86,7 +86,7 @@ public class MultiRuntimeComponentDefinition extends AbstractComponentDefinition
         case VERSION_0_2:
             try {
                 result = new JarRuntimeInfo(
-                        new URL("mvn:org.talend.components/test-multiple-runtime-comp-runtime-v02/0.1.0-SNAPSHOT"),
+                        new URL("mvn:org.talend.components/test-multiple-runtime-comp-runtime-v02"),
                         DependenciesReader.computeDependenciesFilePath("org.talend.components",
                                 "test-multiple-runtime-comp-runtime-v02"),
                         "org.talend.multiruntime.MultiRuntimeComponentSource");
