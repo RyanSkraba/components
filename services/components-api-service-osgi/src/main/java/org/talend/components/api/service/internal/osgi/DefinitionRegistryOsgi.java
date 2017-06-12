4c49e1cd8bfe0c84ea956fdffb68121a0804900c
nrousseau <nrousseau@talend.com>
2017-06-12 23:19:04 +0800

    feat(TCOMP-568) add service tracker (#821)
    * feat(TCOMP-568) add service tracker
    
    * feat(TCOMP-568):add a unit test
    
    * feat(TCOMP-568):fix unit tests cyclic deps

--- services/components-api-service-osgi/src/main/java/org/talend/components/api/service/internal/osgi/DefinitionRegistryOsgi.java
+++ services/components-api-service-osgi/src/main/java/org/talend/components/api/service/internal/osgi/DefinitionRegistryOsgi.java
@@ -12,13 +12,11 @@
 // ============================================================================
 package org.talend.components.api.service.internal.osgi;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.Map;
-
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.InvalidSyntaxException;
 import org.osgi.framework.ServiceReference;
+import org.osgi.util.tracker.ServiceTracker;
+import org.osgi.util.tracker.ServiceTrackerCustomizer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.talend.components.api.ComponentInstaller;
@@ -27,44 +25,52 @@ import org.talend.daikon.definition.service.DefinitionRegistryService;
 
 import aQute.bnd.annotation.component.Activate;
 import aQute.bnd.annotation.component.Component;
+import aQute.bnd.annotation.component.Deactivate;
 
 @Component(provide = { DefinitionRegistry.class, DefinitionRegistryService.class })
 public class DefinitionRegistryOsgi extends DefinitionRegistry {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(DefinitionRegistryOsgi.class);
 
-    protected static <T> Map<String, T> populateMap(BundleContext bc, Class<T> cls) {
-        Map<String, T> map = new HashMap<>();
-        try {
-            String typeCanonicalName = cls.getCanonicalName();
-            Collection<ServiceReference<T>> serviceReferences = bc.getServiceReferences(cls, null);
-            for (ServiceReference<T> sr : serviceReferences) {
-                T service = bc.getService(sr);
-                Object nameProp = sr.getProperty("component.name"); //$NON-NLS-1$
-                if (nameProp instanceof String) {
-                    map.put((String) nameProp, service);
-                    LOGGER.info("Registered the component: " + nameProp + "(" + service.getClass().getCanonicalName() + ")"); //$NON-NLS-1$//$NON-NLS-2$//$NON-NLS-3$
-                } else {// no name set so issue a warning
-                    LOGGER.warn("Failed to register the following component because it is unnamed: " //$NON-NLS-1$
-                            + service.getClass().getCanonicalName());
-                }
-            }
-            if (map.isEmpty()) {// warn if no components were registered
-                LOGGER.warn("Could not find any registered components for type :" + typeCanonicalName); //$NON-NLS-1$
-            } // else everything is fine
-        } catch (InvalidSyntaxException e) {
-            LOGGER.error("Failed to get " + cls.getSimpleName() + " services", e); //$NON-NLS-1$
-        }
-        return map;
-    }
+    private ServiceTracker<ComponentInstaller, ComponentInstaller> serviceTracker;
 
     @Activate
-    void activate(BundleContext bundleContext) throws InvalidSyntaxException {
-        Map<String, ComponentInstaller> installers = populateMap(bundleContext, ComponentInstaller.class);
-        for (ComponentInstaller installer : installers.values()) {
-            installer.install(this);
-        }
-        this.lock();
+    void activate(final BundleContext bundleContext) throws InvalidSyntaxException {
+        serviceTracker = new ServiceTracker<>(bundleContext, ComponentInstaller.class.getName(),
+                new ServiceTrackerCustomizer<ComponentInstaller, ComponentInstaller>() {
+
+                    @Override
+                    public ComponentInstaller addingService(ServiceReference<ComponentInstaller> serviceRef) {
+                        ComponentInstaller componentInstaller = bundleContext.getService(serviceRef);
+                        Object nameProp = serviceRef.getProperty("component.name"); //$NON-NLS-1$
+                        if (nameProp instanceof String) {
+                            LOGGER.info("Registered the component: " + nameProp + "(" //$NON-NLS-1$//$NON-NLS-2$
+                                    + componentInstaller.getClass().getCanonicalName() + ")"); //$NON-NLS-1$
+                            componentInstaller.install(DefinitionRegistryOsgi.this);
+                        } else {// no name set so issue a warning
+                            LOGGER.warn("Failed to register the following component because it is unnamed: " //$NON-NLS-1$
+                                    + serviceRef.getClass().getCanonicalName());
+                        }
+                        return componentInstaller;
+                    }
+
+                    @Override
+                    public void modifiedService(ServiceReference<ComponentInstaller> arg0,
+                            ComponentInstaller componentInstaller) {
+                        // not handled for now
+                    }
+
+                    @Override
+                    public void removedService(ServiceReference<ComponentInstaller> arg0, ComponentInstaller componentInstaller) {
+                        // No any un-install yet from the service
+                    }
+                });
+        serviceTracker.open();
+    }
+
+    @Deactivate
+    void deactivate(BundleContext bundleContext) throws Exception {
+        serviceTracker.close();
     }
 
 }
