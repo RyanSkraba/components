4833bfbe7c276c681fcafc48e3f434ecb99c21fd
Dmytro Sylaiev <dmytro-sylaiev@users.noreply.github.com>
2017-07-12 17:24:58 +0300

    fix(TDI-39134): Fix processing null BigDecimal (#893)
    * fix(TDI-39134): Fix processing null BigDecimal
    
    * fix(TDI-39134): Minor changes

--- components/components-snowflake/src/main/java/org/talend/components/snowflake/runtime/SnowflakeAvroRegistry.java
+++ components/components-snowflake/src/main/java/org/talend/components/snowflake/runtime/SnowflakeAvroRegistry.java
@@ -12,6 +12,7 @@
 // ============================================================================
 package org.talend.components.snowflake.runtime;
 
+import java.math.BigDecimal;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -110,7 +111,12 @@ public class SnowflakeAvroRegistry extends JDBCAvroRegistry {
                 @Override
                 public Object convertToAvro(ResultSet value) {
                     try {
-                        return value.getBigDecimal(f.pos() + 1).toString();
+                        BigDecimal bigDecimalValue = value.getBigDecimal(f.pos() + 1);
+                        if (null != bigDecimalValue) {
+                            return bigDecimalValue.toString();
+                        }
+
+                        return null;
                     } catch (SQLException e) {
                         throw new ComponentException(e);
                     }
