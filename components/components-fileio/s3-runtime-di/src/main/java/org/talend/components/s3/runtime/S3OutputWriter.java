2f82959baf7d396ecd288458bc819f44818b9951
Ivan Gonchar <gonchar-ivan@users.noreply.github.com>
2017-10-05 13:52:57 +0300

    fix(TCOMP-633): replace constant temp file name with generated (#974)

--- components/components-fileio/s3-runtime-di/src/main/java/org/talend/components/s3/runtime/S3OutputWriter.java
+++ components/components-fileio/s3-runtime-di/src/main/java/org/talend/components/s3/runtime/S3OutputWriter.java
@@ -72,9 +72,7 @@ public class S3OutputWriter implements Writer<Result> {
         }
 
         // prepare the local target, will upload it to s3 and clear it in the close method
-        String tmpDir = System.getProperty("java.io.tmpdir");
-        data_file = new File(tmpDir, "data.csv");
-
+        data_file = File.createTempFile("s3-", ".csv");
         data_file.deleteOnExit();
 
         OutputStream outputStream = new FileOutputStream(data_file);
