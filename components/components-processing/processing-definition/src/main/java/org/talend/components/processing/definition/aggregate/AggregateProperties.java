38136bcc43070d1163564e44bd51b3f4e6477a30
Pierre Bailly-Ferry <pbailly@talend.com>
2018-02-22 20:20:18 +0100

    feat(TFD-2986): Improve the UI of Aggregate components. (#1148)
    * feat(TFD-2986): Improve the UI of Aggregate components.
    
    * feat(TFD-2986): Set minElement to 0.
    
    * feat(TFD-2986): Improve aggregate usage.
    
    * feat(TFD-2986): Improve UI.

--- components/components-processing/processing-definition/src/main/java/org/talend/components/processing/definition/aggregate/AggregateProperties.java
+++ components/components-processing/processing-definition/src/main/java/org/talend/components/processing/definition/aggregate/AggregateProperties.java
@@ -85,16 +85,18 @@ public class AggregateProperties extends FixedConnectorsComponentProperties {
     public void setupLayout() {
         super.setupLayout();
         Form mainForm = new Form(this, Form.MAIN);
-        mainForm.addRow(Widget.widget(groupBy).setWidgetType(Widget.NESTED_PROPERTIES).setConfigurationValue(
-                Widget.NESTED_PROPERTIES_TYPE_OPTION, "filter")); // TODO correct the value of filter when it available
-        mainForm.addRow(Widget.widget(operations).setWidgetType(Widget.NESTED_PROPERTIES).setConfigurationValue(
-                Widget.NESTED_PROPERTIES_TYPE_OPTION, "filter")); // TODO correct the value of filter when it available
+
+        mainForm.addRow(Widget.widget(groupBy).setWidgetType(Widget.UNCOLLAPSIBLE_NESTED_PROPERTIES)
+                .setConfigurationValue(Widget.NESTED_PROPERTIES_TYPE_OPTION, "groupment parameter"));
+        mainForm.addRow(Widget.widget(operations).setWidgetType(Widget.COLUMNS_PROPERTIES)
+                .setConfigurationValue(Widget.NESTED_PROPERTIES_TYPE_OPTION, "operation"));
     }
 
     @Override
     public void setupProperties() {
         super.setupProperties();
         groupBy.init();
+        groupBy.setMinItems("0");
         groupBy.createAndAddRow();
         operations.init();
         operations.createAndAddRow();
