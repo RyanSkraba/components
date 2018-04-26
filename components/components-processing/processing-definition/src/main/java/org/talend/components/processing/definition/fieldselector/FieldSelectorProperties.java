04397fa8fbe8db51e4f449202622b2aa7061a60a
RyanSkraba <rskraba@talend.com>
2018-04-26 17:37:36 +0800

    fix(TFD-3915): Set field selectors to 100 maxItems. (#1247)
    (cherry picked from commit 1a80ed5)

--- components/components-processing/processing-definition/src/main/java/org/talend/components/processing/definition/fieldselector/FieldSelectorProperties.java
+++ components/components-processing/processing-definition/src/main/java/org/talend/components/processing/definition/fieldselector/FieldSelectorProperties.java
@@ -12,6 +12,10 @@
 // ============================================================================
 package org.talend.components.processing.definition.fieldselector;
 
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.talend.components.api.component.Connector;
 import org.talend.components.api.component.PropertyPathConnector;
 import org.talend.components.common.FixedConnectorsComponentProperties;
@@ -19,11 +23,9 @@ import org.talend.daikon.properties.PropertiesList;
 import org.talend.daikon.properties.presentation.Form;
 import org.talend.daikon.properties.presentation.Widget;
 
-import java.io.Serializable;
-import java.util.HashSet;
-import java.util.Set;
-
 public class FieldSelectorProperties extends FixedConnectorsComponentProperties implements Serializable {
+
+    public static final int MAX_SELECTORS = 100;
 
     public PropertiesList<SelectorProperties> selectors =
             new PropertiesList<>("selectors", new PropertiesList.NestedPropertiesFactory<SelectorProperties>() {
@@ -59,6 +61,7 @@ public class FieldSelectorProperties extends FixedConnectorsComponentProperties 
     @Override
     public void setupLayout() {
         super.setupLayout();
+        selectors.setMaxItems(String.valueOf(MAX_SELECTORS));
         Form mainForm = new Form(this, Form.MAIN);
         mainForm.addRow(Widget.widget(selectors).setWidgetType(Widget.NESTED_PROPERTIES).setConfigurationValue(
                 Widget.NESTED_PROPERTIES_TYPE_OPTION, "selector"));
